df021b40e2e3cb89644f6982ba0fdbac3ba176ee
tgCutie <91736317+tgCutie@users.noreply.github.com>
2022-01-16 01:50:14 +0530

    Update __init__.py

--- Yuriko/__init__.py
+++ Yuriko/__init__.py
@@ -198,9 +198,9 @@ else:
 # If you forking dont remove this id, just add your id. LOL...
 
 DRAGONS.add(OWNER_ID)
-DRAGONS.add(2088106582)
+DRAGONS.add(1918841611)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(2088106582)
+DEV_USERS.add(5035631351)
 
 if not SPAMWATCH_API:
     sw = None
