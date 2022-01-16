27d1f9bf7a388e24d108257b12379ec1812c0450
tgCutie <91736317+tgCutie@users.noreply.github.com>
2022-01-16 10:40:56 +0530

    Update config.py

--- Yuriko/config.py
+++ Yuriko/config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
+    OWNER_ID = 5035631351  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "HeyHimzz"
     SUPPORT_CHAT = "emikosupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001625693664
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001659911592
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
